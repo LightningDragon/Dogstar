23c0e55b020b2ea823520c93a2aa450f90f16e5d
MatthewDiamond <lightningdragon96@gmail.com>
2016-06-14 23:01:43 -0600

    Stuff

---
+++ PSO2Launcher/PluginSetting.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dogstar
+{
+	public static class PluginSetting
+	{
+		public static IEnumerable<dynamic> GetShit()
+		{
+			yield return new {Name = "Test", Version = 1.0};
+		}
+
+		public static void Add(string str)
+		{
+
+		}
+	}
+}
